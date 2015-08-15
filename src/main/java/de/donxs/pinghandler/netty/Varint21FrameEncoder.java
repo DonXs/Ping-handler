dcc5cdef99b2ba9cc34b96d5ba33121724e19e39
Lukas <DonXsLP@web.de>
2015-08-15 17:09:05 +0200

    Moved varint-size method into encode.

--- src/main/java/de/donxs/pinghandler/netty/Varint21FrameEncoder.java
+++ src/main/java/de/donxs/pinghandler/netty/Varint21FrameEncoder.java
@@ -5,39 +5,33 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
+
 @ChannelHandler.Sharable
-public class Varint21FrameEncoder extends MessageToByteEncoder<ByteBuf>
-{
+public class Varint21FrameEncoder extends MessageToByteEncoder<ByteBuf> {
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception
-    {
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception {
+
         int bodyLen = msg.readableBytes();
-        int headerLen = varintSize( bodyLen );
-        out.ensureWritable( headerLen + bodyLen );
+        int headerLen = 5;
 
-        NettyUtil.writeVarInt( bodyLen, out );
-        out.writeBytes( msg );
+        if ((bodyLen & 0xFFFFFF80) == 0) {
+            headerLen = 1;
+        }
+        if ((bodyLen & 0xFFFFC000) == 0) {
+            headerLen = 2;
+        }
+        if ((bodyLen & 0xFFE00000) == 0) {
+            headerLen = 3;
+        }
+        if ((bodyLen & 0xF0000000) == 0) {
+            headerLen = 4;
+        }
+
+        out.ensureWritable(headerLen + bodyLen);
+
+        NettyUtil.writeVarInt(bodyLen, out);
+        out.writeBytes(msg);
     }
 
-    private static int varintSize(int paramInt)
-    {
-        if ( ( paramInt & 0xFFFFFF80 ) == 0 )
-        {
-            return 1;
-        }
-        if ( ( paramInt & 0xFFFFC000 ) == 0 )
-        {
-            return 2;
-        }
-        if ( ( paramInt & 0xFFE00000 ) == 0 )
-        {
-            return 3;
-        }
-        if ( ( paramInt & 0xF0000000 ) == 0 )
-        {
-            return 4;
-        }
-        return 5;
-    }
 }
