cc6df58b416910bf4d54de43adf512c7d2f45195
Lukas <DonXsLP@web.de>
2015-08-15 17:12:04 +0200

    Tidied up code.

--- src/main/java/de/donxs/pinghandler/netty/NettyUtil.java
+++ src/main/java/de/donxs/pinghandler/netty/NettyUtil.java
@@ -8,14 +8,17 @@ import io.netty.buffer.ByteBuf;
 public class NettyUtil {
 
     public static void writeString(String s, ByteBuf buf) {
+
         Preconditions.checkArgument(s.length() <= Short.MAX_VALUE, "Cannot send string longer than Short.MAX_VALUE (got %s characters)", s.length());
 
         byte[] b = s.getBytes(Charsets.UTF_8);
         writeVarInt(b.length, buf);
         buf.writeBytes(b);
+
     }
 
     public static String readString(ByteBuf buf) {
+
         int len = readVarInt(buf);
         Preconditions.checkArgument(len <= Short.MAX_VALUE, "Cannot receive string longer than Short.MAX_VALUE (got %s characters)", len);
 
@@ -23,6 +26,7 @@ public class NettyUtil {
         buf.readBytes(b);
 
         return new String(b, Charsets.UTF_8);
+
     }
 
     public static int readVarInt(ByteBuf input) {
@@ -30,6 +34,7 @@ public class NettyUtil {
     }
 
     public static int readVarInt(ByteBuf input, int maxBytes) {
+
         int out = 0;
         int bytes = 0;
         byte in;
@@ -48,14 +53,17 @@ public class NettyUtil {
         }
 
         return out;
+
     }
 
     public static void writeVarInt(int value, ByteBuf output) {
+
         int part;
         while (true) {
             part = value & 0x7F;
 
             value >>>= 7;
+
             if (value != 0) {
                 part |= 0x80;
             }
@@ -65,7 +73,9 @@ public class NettyUtil {
             if (value == 0) {
                 break;
             }
+
         }
+
     }
 
 }
--- src/main/java/de/donxs/pinghandler/netty/Varint21FrameDecoder.java
+++ src/main/java/de/donxs/pinghandler/netty/Varint21FrameDecoder.java
@@ -7,48 +7,52 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.CorruptedFrameException;
 import java.util.List;
 
-public class Varint21FrameDecoder extends ByteToMessageDecoder
-{
+
+public class Varint21FrameDecoder extends ByteToMessageDecoder {
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
-    {
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+
         in.markReaderIndex();
 
-        final byte[] buf = new byte[ 3 ];
-        for ( int i = 0; i < buf.length; i++ )
-        {
-            if ( !in.isReadable() )
-            {
+        final byte[] buf = new byte[3];
+
+        for (int i = 0; i < buf.length; i++) {
+
+            if (!in.isReadable()) {
                 in.resetReaderIndex();
                 return;
             }
 
             buf[i] = in.readByte();
-            if ( buf[i] >= 0 )
-            {
-                int length = NettyUtil.readVarInt( Unpooled.wrappedBuffer( buf ) );
-                if ( length == 0 )
-                {
-                    throw new CorruptedFrameException( "Empty Packet!" );
+
+            if (buf[i] >= 0) {
+
+                int length = NettyUtil.readVarInt(Unpooled.wrappedBuffer(buf));
+
+                if (length == 0) {
+                    throw new CorruptedFrameException("Empty Packet!");
                 }
 
-                if ( in.readableBytes() < length )
-                {
+                if (in.readableBytes() < length) {
                     in.resetReaderIndex();
                     return;
-                } else
-                {
-                    // TODO: Really should be a slice!
-                    ByteBuf dst = ctx.alloc().directBuffer( length );
-                    in.readBytes( dst );
-
-                    out.add( dst );
-                    return;
                 }
+                else {
+
+                    ByteBuf dst = ctx.alloc().directBuffer(length);
+                    in.readBytes(dst);
+
+                    out.add(dst);
+                    return;
+
+                }
+
             }
+
         }
 
-        throw new CorruptedFrameException( "length wider than 21-bit" );
+        throw new CorruptedFrameException("length wider than 21-bit");
     }
+
 }
